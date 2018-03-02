b68b59bbb89d4af3aacaaabe054445f3e4adce12
Arslan Tachmuradov <arslan.tachmuradov@gmail.com>
2018-03-03 00:26:52 +0300

    String-generator task

---
+++ string-generator/src/index.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'form.html';
+
+function retranslate($example) : string
+{
+    $modifiedExample = "";
+
+    function str_generator($example) {
+        static $changes = 0;
+
+        for ($i = 0; $i < strlen($example); $i++) {
+            switch ($example[$i]) {
+                case "h":
+                    $changes++;
+                    yield "4";
+                    break;
+                case "l":
+                    $changes++;
+                    yield "1";
+                    break;
+                case "e":
+                    $changes++;
+                    yield "3";
+                    break;
+                case "o":
+                    $changes++;
+                    yield "0";
+                    break;
+                default:
+                    yield $example[$i];
+                    break;
+            }
+        }
+
+        return $changes;
+    }
+
+    $generator = str_generator($example);
+
+    foreach ($generator as $i) {
+        $modifiedExample .= $i;
+    }
+
+    echo "Число изменений: " .$generator->getReturn() ."<br>";
+
+    return $modifiedExample;
+}
+
+$input = "";
+
+if (isset($_POST['msg'])) {
+    $input = $_POST['msg'];
+}
+
+echo "Рузультат: " .retranslate($input);
