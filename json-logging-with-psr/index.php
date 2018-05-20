74196979a00263ebead326cb6673e771c3876499
Arslan Tachmuradov <arslan.tachmuradov@gmail.com>
2018-05-20 16:15:08 +0300

    Task 13 - JsonLogger with PSR

---
+++ json-logging-with-psr/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arslan
+ * Date: 07.05.2018
+ * Time: 22:29
+ */
+
+function a() {
+   echo 'hello';
+}
---
+++ json-logging-with-psr/src/Logger/JsonLogger.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arslan
+ * Date: 20.05.2018
+ * Time: 14:38
+ */
+
+namespace Logger;
+
+
+class JsonLogger
+{
+
+    /**
+     * JsonLogger constructor.
+     */
+    public function __construct()
+    {
+    }
+}
